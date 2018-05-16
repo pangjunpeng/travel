3db05699176cf29f44e3645b5c8f3f004d90aa79
pangjunpeng <oqpqw5@163.com>
2018-05-16 15:27:15 +0800

    修改build路径&webpack一系列配置

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="div">
+    Home
+  </div>
+</template>
+<script>
+  export default{
+    name: 'Home',
+    data () {
+      return {}
+    }
+  }
+</script>
+<style scoped lang="less" type="text/less">
+</style>
